ac5ff5b6e114e5bba4615f6a56976fcc822ae36d
Evan Barry <Evan.Barry@students.ittralee.ie>
2019-03-20 12:54:13 +0000

    Completed map Add collision with walls
    Signed-off-by: Evan Barry <Evan.Barry@students.ittralee.ie>

---
+++ Practical Gaming Project/Assets/scripts/SpawnMap.cs
@@ -0,0 +1,20 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class SpawnMap : MonoBehaviour {
+
+    public GameObject map;
+
+	// Use this for initialization
+	void Start () {
+
+        Instantiate(map);
+
+	}
+	
+	// Update is called once per frame
+	void Update () {
+		
+	}
+}
